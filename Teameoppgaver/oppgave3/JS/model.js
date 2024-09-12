a57598110952dee8a3a269bb91251edb1b5dec27
haakonerol <haakon.erol@gmail.com>
2024-09-12 11:13:51 +0200

    update

---
+++ Teameoppgaver/oppgave3/JS/model.js
@@ -0,0 +1,5 @@
+let message="";
+let fuelTimer;
+
+let coolnessProgress=50;
+let fuelProgress=100;
--- Teameoppgaver/oppgave3/JS/view.js
+++ Teameoppgaver/oppgave3/JS/view.js
@@ -1,33 +1,35 @@
+
+
 function updateView() {
   document.getElementById("app").innerHTML = `
-        <div id="coolness">
-          <div id="coolnessBar">50%</div>
-          </div>
-          <div id="fuel">
-          <div id="fuelBar">100%</div>
-          </div>
-          <div id="congrats">
+        <div id="coolness">Kulhet:
+          <div id="coolnessBar">${coolnessProgress}%</div>
+        </div>
+        <div id="fuel">Bensin:
+          <div id="fuelBar">${fuelProgress}%</div>
+        </div>
+        <div id="congrats">
             <h1> Gratulerer! du vant</h1>                
-          </div>
-          <div>                                   
-          <img id="car" src="./images/car.gif"  alt="car"/>
-          <img class="kompis" src="./images/images.jpg" alt="kompis"/>
+        </div>
+        <div class="main">                                   
+          <img id="car" src="../images/car.gif"  alt="car"/>
+          <img class="kompis" src="../images/images.jpg" alt="kompis"/>
           
-          </div>            
-          <p id="message">${message}</p>
+        </div>            
+          <p id="message"></p>
           
           <button class="button" onclick="drive()"> KJØR Forbi</button>
           <button class="button" onclick="receive()"> Ta i Mot</button>
-          <div id="dropdown" >
-          <button class="button" onclick="greet()"> Hilse </button>
-            <ul id="dyrList">
-              <li>sdfghjk</li>
-              <li>sdfghjk</li>
-              <li>sdfghjk</li>
-              <li>sdfghjk</li>
+        <div id="dropdown" >
+          <button class="button" onclick="toggleDropdown()"> Hilse </button>
+            <ul id="hilsList">
+              <li onclick="greet()">Assen går det!</li>
+              <li onclick="greet()">Hva Skjera!</li>
+              <li onclick="greet()">Wazzaaap!</li>
+              <li onclick="greet()">Kom på besøk!</li>
             </ul>
-          </div>
+        </div>
           `;
 }
-console.log(document.getElementById("app"));
+
 updateView();
