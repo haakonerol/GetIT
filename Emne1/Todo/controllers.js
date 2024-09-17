18b60622a475eff9fee835bd66c273adf375f872
haakonerol <haakon.erol@gmail.com>
2024-09-17 14:59:07 +0200

    new columns added

--- Emne1/Todo/controllers.js
+++ Emne1/Todo/controllers.js
@@ -1,18 +1,42 @@
 let toDoDescriptionInput = document.getElementById("toDoDescription");
+let personName = document.getElementById("toDoPerson");
+
+function activateAddBtn() {
+  if (toDoDescriptionInput.value.trim() === "") {
+    document.getElementById("add").disabled = true;
+  } else {
+    document.getElementById("add").disabled = false;
+  }
+}
 
 function addToDo() {
+  let addDate = new Date().toLocaleDateString("nb-NO");
   toDos.push({
     description: toDoDescriptionInput.value,
+    taskOfPerson: personName.value,
+    dayOfAdd: addDate,
+    dayOfComplited: "",
     isDone: false,
   });
   toDoDescriptionInput.value = "";
+  personName.value = "";
+  activateAddBtn();
   showTable();
 }
 
 function changeIsDone(checkbox, index) {
+  let addDate = new Date().toLocaleDateString("nb-NO");
   const isDone = checkbox.checked;
   // console.log(isDone);
   toDos[index].isDone = isDone;
+  if (toDos[index].isDone) {
+    toDos[index].dayOfComplited = addDate;
+  } else {
+    toDos[index].dayOfComplited = "";
+  }
+  //   console.log(toDos[index].dayOfComplited);
+  //   console.log(toDos[index].isDone);
+  showTable();
 }
 function deleteToDo(index) {
   toDos.splice(index, 1);
@@ -22,11 +46,14 @@ function editToDo(index) {
   toDos[index].editMode = true;
   showTable();
 }
-function updateToDo(index){
-    const id= `updateToDo${index}`
-    const inputTag = document.getElementById(id)
-    toDos[index].description = inputTag.value
-    toDos[index].editMode = false;
-    showTable();
-    //console.log(toDos);
+function update(index) {
+  const idToDo = `updateToDo${index}`;
+  const idPerson = `updatePerson${index}`;
+  const inputToDo = document.getElementById(idToDo);
+  const inputPerson = document.getElementById(idPerson);
+  toDos[index].description = inputToDo.value;
+  toDos[index].taskOfPerson = inputPerson.value;
+  toDos[index].editMode = false;
+  showTable();
+  //console.log(toDos);
 }
--- Emne1/Todo/view.js
+++ Emne1/Todo/view.js
@@ -3,7 +3,10 @@ function showTable() {
   let html = `
     <tr>
      <th>Oppgave</th>
+     <th>Lagt tid</th>
+     <th>Person</th>
      <th>Gjort</th>
+     <th>Gjort tid</th>
      <th></th>
     </tr>`;
   for (let i = 0; i < toDos.length; i++) {
@@ -19,7 +22,10 @@ function createHtmlRow(i) {
     return `
     <tr>
      <td>${toDo.description}</td>
+     <td>${toDo.dayOfAdd??""}</td>
+     <td>${toDo.taskOfPerson?? " "}</td>
      <td><input onchange="changeIsDone(this,${i})" type="checkbox" ${checkedHtml}></td>
+     <td>${toDo.dayOfComplited??""}</td>
      <td>
         <button onclick="deleteToDo(${i})">Erase</button>
         <button onclick="editToDo(${i})">Edit</button>
@@ -30,9 +36,12 @@ function createHtmlRow(i) {
   return `
     <tr>
      <td><input id="updateToDo${i}" type="text" value="${toDo.description}"/></td>
+     <td>${toDo.dayOfAdd??""}</td>
+     <td><input id="updatePerson${i}" type="text" value="${toDo.taskOfPerson??" "}"/></td>
      <td><input onchange="changeIsDone(this,${i})" type="checkbox" ${checkedHtml}></td>
+     <td>${toDo.dayOfComplited??""}</td>
      <td>
-        <button onclick="updateToDo(${i})">Save</button>
+        <button onclick="update(${i})">Save</button>
         
      </td>
     </tr>
