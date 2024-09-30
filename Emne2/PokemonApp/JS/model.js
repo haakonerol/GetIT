ec55f185db1c5c042e8e61c905adaf79984b728f
haakonerol <haakon.erol@gmail.com>
2024-09-30 10:51:32 +0200

    pokemon app has began

---
+++ Emne2/PokemonApp/JS/model.js
@@ -0,0 +1,18 @@
+let app = document.getElementById("app");
+
+let players = [
+    {playerName:"Abi",playerImage:"",playerPokemons:[]},
+    {playerName:"Loe",playerImage:"",playerPokemons:[]},
+    {playerName:"Christoffer",playerImage:"",playerPokemons:[]},
+    {playerName:"Hakan",playerImage:"",playerPokemons:[]},
+];
+
+let pokemons = [
+  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+];
