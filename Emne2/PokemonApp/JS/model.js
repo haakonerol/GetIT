0b64da0b62e342cc3876941aafff462c745bc9b0
haakonerol <haakon.erol@gmail.com>
2024-09-30 11:41:22 +0200

    images added

--- Emne2/PokemonApp/JS/model.js
+++ Emne2/PokemonApp/JS/model.js
@@ -1,18 +1,18 @@
 let app = document.getElementById("app");
 
 let players = [
-    {playerName:"Abi",playerImage:"",playerPokemons:[]},
-    {playerName:"Loe",playerImage:"",playerPokemons:[]},
-    {playerName:"Christoffer",playerImage:"",playerPokemons:[]},
-    {playerName:"Hakan",playerImage:"",playerPokemons:[]},
+    {id:1,playerName:"Abi",playerImage:"",playerPokemons:[]},
+    {id:2,playerName:"Loe",playerImage:"",playerPokemons:[]},
+    {id:3,playerName:"Christoffer",playerImage:"",playerPokemons:[]},
+    {id:4,playerName:"Hakan",playerImage:"",playerPokemons:[]},
 ];
 
 let pokemons = [
-  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
-  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
-  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
-  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
-  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
-  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
-  { pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { id:1,pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { id:2,pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { id:3,pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { id:4,pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { id:5,pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { id:6,pokemonName: "Pikachu", health: 140, level: 14, image: "" },
+  { id:7,pokemonName: "Pikachu", health: 140, level: 14, image: "" },
 ];
---
+++ Emne2/PokemonApp/JS/view.js
@@ -0,0 +1,24 @@
+
+function updateView(){
+    app.innerHTML= `
+    
+        <div class="container">
+            <div class="pokemon">
+                <img src="" alt="">
+                <h3>Name</h3>
+                <h3>Level:</h3>
+            </div>
+            <div class="player">
+                <img src="" alt="">
+                <h3>Name</h3>
+            </div>
+            <div class="buttons">
+                <button onclick="">Fang</button>
+                <button onclick="">Finn en annen</button>
+                <button onclick="">Vis dine pokemon</button>
+            </div>
+        </div>
+    
+    `
+};
+updateView();
